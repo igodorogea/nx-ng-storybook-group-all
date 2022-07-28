c4882b6d1c225dd79b6f15982776ca4e7c10e601
Ivan Godorogea <ivan.godorogea@codefactorygroup.com>
2022-07-28 16:47:44 +0300

    feat(optional): move storybook configuration from `foo` app to a dedicated lib

--- libs/root/storybook/main.js
+++ libs/root/storybook/main.js
@@ -2,25 +2,19 @@ const rootMain = require('../../../.storybook/main');
 
 module.exports = {
   ...rootMain,
-
   core: { ...rootMain.core, builder: 'webpack5' },
-
   stories: [
     ...rootMain.stories,
-    '../src/app/**/*.stories.mdx',
-    '../src/app/**/*.stories.@(js|jsx|ts|tsx)',
-    '../../../libs/**/*.stories.mdx',
-    '../../../libs/**/*.stories.@(js|jsx|ts|tsx)',
+    '../../**/*.stories.mdx',
+    '../../**/*.stories.@(js|jsx|ts|tsx)',
+    '../../../apps/foo/src/app/**/*.stories.mdx',
+    '../../../apps/foo/src/app/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [...rootMain.addons],
   webpackFinal: async (config, { configType }) => {
-    // apply any global webpack configs that might have been specified in .storybook/main.js
     if (rootMain.webpackFinal) {
       config = await rootMain.webpackFinal(config, { configType });
     }
-
-    // add your own webpack tweaks if needed
-
     return config;
   },
 };
